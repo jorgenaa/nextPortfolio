4d0b831f63c8a48f7c8c0a70a6dc97f511105e7f
Jorgen Arnes <jorgen.arnes@gmail.com>
2022-04-20 14:03:19 +0200

    changed the export

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 export { default as Layout } from './Layout'
 export { default as Hero } from './Hero'
-export { default as Nav } from './Nav'
+export { Nav } from './Nav'
+export { Navbar } from './Nav'
 export { default as Header } from './Header'
 export { default as Footer } from './Footer'
 export { default as About } from './About'
