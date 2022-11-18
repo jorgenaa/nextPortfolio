4ff96cbeb7565d53242628db9891fe01492bcce0
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2022-11-19 00:35:31 +0100

    added a new asset for the portfolio section

--- assets/assets.ts
+++ assets/assets.ts
@@ -1,8 +1,15 @@
 
 import CodeImg from './img/code_1024x768.jpg';
-import HolidazeImg from './img/bryggen.jpg';
+
 
 export const assets = [
+	{
+		title: 'Eccomerce website',
+		icon: 'assets/icons/GitHub.png',
+		img: CodeImg,
+		github: '',
+		website: 'https://tilbehoret.no/'
+	},
 	{	
 		title: 'Food ordering site',
 		icon: 'assets/icons/GitHub.png',
@@ -16,13 +23,6 @@ export const assets = [
 		img: CodeImg,
 		github: 'https://github.com/jorgenaa/nextjs_redux_todo-app',
 		website: 'https://nextjsreduxtodoapp.vercel.app/',
-	},
-	{
-		title: 'Booking site',
-		icon: 'assets/icons/GitHub.png',
-		img: HolidazeImg,
-		github: 'https://github.com/jorgenaa/project-exam-2',
-		website: 'https://holidaze-project-exam-2.netlify.app/',
 	},
 	{
 		title: 'Book Favourite site',
